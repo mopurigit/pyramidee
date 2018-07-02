16467e9de6e69b58a3d199831a23aa8cd95aaec2
Chandra <mopuri.4156@gmail.com>
2018-07-02 11:26:57 +0530

    This is new space

---
+++ Desktop/devops/project/src/main/java/chandra/App.java
@@ -0,0 +1,13 @@
+package chandra;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
